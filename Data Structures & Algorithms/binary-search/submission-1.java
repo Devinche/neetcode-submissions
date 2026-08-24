a86fadf55105553d84a2d2d5149482c59090cc85
Devansh Singh <98658538+Devinche@users.noreply.github.com>
2026-08-24 16:23:12 -0400

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        //HashSet<Integer> set = new HashSet<>();
+
+        // for (int num : nums) {
+
+        //     if (num == target) {
+
+        //         return  - 1;
+        //     }
+
+        // }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == target) {
+
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
